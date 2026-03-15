cf2458d2123dd81e010783376532fc968871d1e6
Leila4242 <aliyevaleyla6277@gmail.com>
2026-03-15 13:52:26 +0000

    done

---
+++ python-hello_world/8-concat_edges.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = str[str.find("object-oriented programming"):str.find("\
+ language")] + " " + str[str.find("with"):str.find("very")] +  str[str.find("Python"):str.find("is")]
+print(str)
