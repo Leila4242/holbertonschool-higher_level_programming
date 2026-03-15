601542faa34d4c279ed41bce6867501fdb5485b1
Leila4242 <aliyevaleyla6277@gmail.com>
2026-03-15 14:42:49 +0000

    done

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"
-str = str[str.find("object-oriented programming"):str.find("l")] + str[str.find("with"):str.find("very")] + str[str.find("Python"):str.find("is")]
+str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
+str = str[39:67] + str[107:112] + str[:6]
 print(str)
