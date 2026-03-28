418e736aef001e898cfc141785bc85246ea6327d
Leila4242 <aliyevaleyla6277@gmail.com>
2026-03-28 18:35:37 +0000

    done

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,14 +1,34 @@
 #!/usr/bin/python3
+"""
+Roman to Integer module
+"""
+
+
 def roman_to_int(roman_string):
-    if roman_string is None or type(roman_string) != str:
+    """
+    Converts a Roman numeral to an integer
+    """
+    if not isinstance(roman_string, str):
         return 0
-    roman_numerals = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+
+    roman_numerals = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+
     result = 0
     n = len(roman_string)
+
     for i in range(n):
-        current_val = roman_numerals.get(roman_string[i], 0)
-        if i + 1 < n and current_val < roman_numerals.get(roman_string[i+1], 0):
-            result -= current_val
+        curr = roman_numerals.get(roman_string[i], 0)
+        if i + 1 < n and curr < roman_numerals.get(roman_string[i + 1], 0):
+            result -= curr
         else:
-            result += current_val
+            result += curr
+
     return result
