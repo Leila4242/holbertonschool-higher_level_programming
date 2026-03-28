8b12776239ee04bd58200600c18687e55d4bba38
Leila4242 <aliyevaleyla6277@gmail.com>
2026-03-28 17:56:18 +0000

    done

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -3,6 +3,7 @@ def best_score(a_dictionary):
     if a_dictionary == None:
         return None
     best = 0
+    best_st = None
     for key, value in a_dictionary.items():
         if best < a_dictionary[key]:
             best = a_dictionary[key]
