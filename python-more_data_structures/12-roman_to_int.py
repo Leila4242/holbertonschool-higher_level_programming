36c45172ae0d277d05921fffeb054c00293ef745
Leila4242 <aliyevaleyla6277@gmail.com>
2026-03-28 18:14:27 +0000

    done

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    result = 0
+    roman_numerals = {
+    'I': 1,
+    'V': 5,
+    'X': 10,
+    'L': 50,
+    'C': 100,
+    'D': 500,
+    'M': 1000
+    }
+    if len(roman_string) == 1:
+        return roman_numerals[roman_string[0]]
+    for i in range(len(roman_string)-1):
+        if roman_numerals[roman_string[i]] < roman_numerals[roman_string[i+1]]:
+            result += roman_numerals[roman_string[i+1]]-roman_numerals[roman_string[i]]
+        else:
+            result += roman_numerals[roman_string[i]]
+    result += roman_numerals[roman_string[len(roman_string)-1]]
+    return result
