0401a7a00c0f5b6d983ff86914b2d29efa4dedc1
Leila4242 <aliyevaleyla6277@gmail.com>
2026-04-12 17:53:47 +0400

    done

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+
 """
 Module for looking up available attributes of an object.
 """
+
+
 def lookup(obj):
     """Returns the list of available attributes of an object."""
     return dir(obj)
