726d8cb666424d0b90e75dcc6986d15568486d84
Leila4242 <aliyevaleyla6277@gmail.com>
2026-04-19 18:15:51 +0000

    done

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,30 @@
+import pickle
+
+
+class CustomObject:
+    def __init__(self, name, age, is_student):
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def display(self):
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"Is Student: {self.is_student}")
+
+    def serialize(self, filename):
+        """Serialize the current instance to a file using pickle."""
+        try:
+            with open(filename, 'wb') as f:
+                pickle.dump(self, f)
+        except (OSError, pickle.PicklingError):
+            return None
+
+    @classmethod
+    def deserialize(cls, filename):
+        """Load and return a CustomObject instance from a pickle file."""
+        try:
+            with open(filename, 'rb') as f:
+                return pickle.load(f)
+        except (OSError, pickle.UnpicklingError):
+            return None
