d79e062fd60a15aa624de9b16b59aa84aaa3fcac
Leila4242 <aliyevaleyla6277@gmail.com>
2026-04-19 18:14:44 +0000

    done

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,13 @@
+import json
+
+
+def serialize_and_save_to_file(data, filename):
+    """Serialize a Python dictionary to a JSON file."""
+    with open(filename, 'w', encoding='utf-8') as f:
+        json.dump(data, f, indent=4)
+
+
+def load_and_deserialize(filename):
+    """Load and deserialize a JSON file into a Python dictionary."""
+    with open(filename, 'r', encoding='utf-8') as f:
+        return json.load(f)
