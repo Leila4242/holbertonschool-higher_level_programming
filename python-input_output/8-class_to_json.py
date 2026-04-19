35586020d100b1bf1d3cd8bd415ed65a23fb8ae6
Leila4242 <aliyevaleyla6277@gmail.com>
2026-04-19 17:51:50 +0000

    done

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-"""Module for converting a class instance to a JSON-serializable dictionary."""
+"""Module for converting a class 
+instance to a JSON-serializable dictionary."""
 
 
 def class_to_json(obj):
-    """Return the dictionary description of an object for JSON serialization."""
+    """Return the dictionary description of object for JSON serialization."""
     return obj.__dict__
