f12f65db571ca028a21be04a7c4c312400a701d1
Leila4242 <aliyevaleyla6277@gmail.com>
2026-04-23 17:39:23 +0400

    done

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -8,10 +8,9 @@ def fetch_and_print_posts():
         print('Status Code: {}'.format(status_code))
 
 def fetch_and_save_posts():
-    # ... məlumatı çəkmə hissəsi ...
+
     posts = r.json()
-    
-    # 1. Yalnız lazım olan sütunları seçərək yeni siyahı hazırlayırıq
+
     data_to_save = []
     for post in posts:
         data_to_save.append({
@@ -20,11 +19,9 @@ def fetch_and_save_posts():
             'body': post['body']
         })
 
-    # 2. Fayla yazma (Ardıcıllığa və writeheader-ə diqqət!)
     with open('posts.csv', 'w', newline='', encoding='utf-8') as f:
-        # fieldnames tam olaraq testin gözlədiyi ardıcıllıqda olmalıdır
         fieldnames = ['id', 'title', 'body']
         writer = csv.DictWriter(f, fieldnames=fieldnames)
         
-        writer.writeheader() # Başlıqları yazır: id,title,body
-        writer.writerows(data_to_save) # Məlumatları yazır
+        writer.writeheader()
+        writer.writerows(data_to_save)
