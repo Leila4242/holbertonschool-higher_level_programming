23eb088ac68f873f077c6505395e3bbb9bb82535
Leila4242 <aliyevaleyla6277@gmail.com>
2026-05-03 12:34:03 +0000

    displaying desciption of table

---
+++ SQL_introduction/5-full_table.sql
@@ -0,0 +1,3 @@
+-- Displaying the description of the table first_table from the database hbtn_0c_0 in my MySQL server.
+-- This script displays the description of table called first_table
+SHOW COLUMNS FROM first_table;
