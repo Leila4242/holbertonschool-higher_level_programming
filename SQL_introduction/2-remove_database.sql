762c03c8a2ea183e91a0bfb5f596282cd82cc6b9
Leila4242 <aliyevaleyla6277@gmail.com>
2026-05-03 11:38:19 +0000

    deleting database if exists

--- SQL_introduction/2-remove_database.sql
+++ SQL_introduction/2-remove_database.sql
@@ -1,3 +1,3 @@
 -- Deleting the database hbtn_0c_0 in my MySQL server
 -- This script deletes database if it was created before
-DELETE DATABASE IF EXISTS hbtn_0c_0
+DROP DATABASE IF EXISTS hbtn_0c_0
